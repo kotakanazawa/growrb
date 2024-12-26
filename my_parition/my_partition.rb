e4d8a992b0751e83692101eb38d24128cda4f269
Kota Kanazawa <kota0818@gmail.com>
2024-12-26 18:39:07 +0900

    refactor

--- my_parition/my_partition.rb
+++ my_parition/my_partition.rb
@@ -1,15 +1,27 @@
+# module Enumerable
+#   def my_partition
+#     result = []
+#     true_array = []
+#     false_array = []
+#     each do |i|
+#       if yield(i)
+#         true_array << i
+#       else
+#         false_array << i
+#       end
+#     end
+#     result.push(true_array, false_array)
+#   end
+# end
+
 module Enumerable
   def my_partition
-    result = []
-    true_array = []
-    false_array = []
-    each do |i|
+    each_with_object([[], []]) do |i, (true_array, false_array)|
       if yield(i)
         true_array << i
       else
         false_array << i
       end
     end
-    result.push(true_array, false_array)
   end
 end
